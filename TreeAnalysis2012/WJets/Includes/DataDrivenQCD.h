f56f3223666c6c035b0e61ea4e70d043fc6c71e7
Apichart Hortiangtham <apichart.hortiangtham@cern.ch>
2014-11-09 23:27:52 +0100

    fix QCD Background calculation

--- TreeAnalysis2012/WJets/Includes/DataDrivenQCD.h
+++ TreeAnalysis2012/WJets/Includes/DataDrivenQCD.h
@@ -6,8 +6,12 @@
 using namespace std;
 
 void DataDrivenQCD( string leptonFlavor = "SMu", int METcut = 0,  int doBJets = 0);
-vector<string> getVectorOfHistoNames();
-void FuncDataDrivenQCD(string variable="ZNGoodJets_Zexc" , string leptonFlavor = "SMu", int METcut = 0, bool doFlat = false, bool doVarWidth = true, int doBJets = 0 );
+
 void FuncOpenAllFiles(string leptonFlavor = "SMu",  int METcut = 0, bool doFlat = false, bool doVarWidth = true , int doBJets = 0 );
+
+vector<string> getVectorOfHistoNames();
+
+void FuncDataDrivenQCD(string variable = "ZNGoodJets_Zexc");
+
 #endif
 
